7dbd35fc5cfe6c09e7ffe9e77b4d8b010b21e956
@hexlet/cli <support@hexlet.io>
2023-11-28 20:15:43 +0300

    submit java-collections-ru/generics

--- java-collections-ru/generics/src/main/java/exercise/App.java
+++ java-collections-ru/generics/src/main/java/exercise/App.java
@@ -3,12 +3,10 @@ package exercise;
 import java.util.List;
 import java.util.Map;
 import java.util.ArrayList;
-import java.util.Map.Entry;
-import java.util.HashMap;
 
 // BEGIN
-class App<K extends String, V extends String> {
-    public List<Map<K, V>> findWhere(List<Map<K, V>> books, Map<K, V> search) {
+class App {
+    public static<K extends String, V extends String> List<Map<K, V>> findWhere(List<Map<K, V>> books, Map<K, V> search) {
         List<Map<K, V>> searchResult1 = new ArrayList<>();
         for (Map<K, V> book : books) {
             if (book.get("author").equals(search.get("author")) && book.get("year").equals(search.get("year"))) {

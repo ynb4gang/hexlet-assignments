32225626ebed88ce9df999f61d4213dee746dc27
@hexlet/cli <support@hexlet.io>
2023-11-29 18:18:44 +0300

    submit java-collections-ru/tests

--- java-collections-ru/tests/src/test/java/exercise/AppTest.java
+++ java-collections-ru/tests/src/test/java/exercise/AppTest.java
@@ -3,39 +3,39 @@ package exercise;
 import static org.assertj.core.api.Assertions.assertThat;
 import java.util.List;
 import java.util.Arrays;
-import java.util.ArrayList;
 import org.junit.jupiter.api.Test;
 
 class AppTest {
 
     @Test
-    void testTake() {
-        // BEGIN
-        @Test
-        void take_ReturnEmptyList_WhenOriginalListIsEmpty() {
-            List<Integer> emptyList = Arrays.asList();
-            List<Integer> result = App.take(emptyList, 2);
-            assertEquals(emptyList,result);
-        }
-        @test
-        void take_ReturnOriginalList_whenOriginalListIsEqual() {
-            List<Integer> smth = Arrays.asList(1,2,3,4);
-            List<Integer> result = App.take(smth, 4);
-            List<Integer> expected = Arrays.asList(1,2,3,4);
-            assertEquals(expected,result);
-        }
-        @Test
-        void take_ReturnCorrectlList_whenOriginalListIsSmaller() {
-            List<Integer> smth = Arrays.asList(3,7,10);
-            List<Integer> result = App.take(smth, 10);
-            assertEquals(smth,result);
-        }
-        @Test
-        void take_ReturnCorrectList_WhenOriginalListIsBigger() {
-            List<Integer> smth = Arrays.asList(5,9,7,6,1);
-            List<Integer> result = App.take(smth, 3);
-            assertEquals(smth(5,9,7),result);
-        }
-        // END
+    void take_ReturnEmptyList_WhenOriginalListIsEmpty() {
+        List<Integer> emptyList = Arrays.asList();
+        List<Integer> result = App.take(emptyList, 2);
+
+        assertThat(result).isEqualTo(emptyList);
+    }
+
+    @Test
+    void take_ReturnOriginalList_whenOriginalListIsEqual() {
+        List<Integer> smth = Arrays.asList(1, 2, 3, 4);
+        List<Integer> result = App.take(smth, 4);
+
+        assertThat(result).isEqualTo(smth);
+    }
+
+    @Test
+    void take_ReturnCorrectList_whenOriginalListIsSmaller() {
+        List<Integer> smth = Arrays.asList(3, 7, 10);
+        List<Integer> result = App.take(smth, 10);
+
+        assertThat(result).isEqualTo(smth);
+    }
+
+    @Test
+    void take_ReturnCorrectList_WhenOriginalListIsBigger() {
+        List<Integer> smth = Arrays.asList(5, 9, 7, 6, 1);
+        List<Integer> result = App.take(smth, 3);
+
+        assertThat(result).isEqualTo(Arrays.asList(5, 9, 7));
     }
 }

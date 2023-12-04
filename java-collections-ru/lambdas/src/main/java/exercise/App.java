f418d859d760671f7a00d712b5254f34c440aaf1
@hexlet/cli <support@hexlet.io>
2023-12-04 14:09:26 +0300

    submit java-collections-ru/lambdas

--- java-collections-ru/lambdas/src/main/java/exercise/App.java
+++ java-collections-ru/lambdas/src/main/java/exercise/App.java
@@ -2,21 +2,22 @@ package exercise;
 
 import java.util.Arrays;
 
-// BEGIN
 class App {
-    public static String[][] DuplicateValue(String[] items){
+    public static String[] duplicateValues(String[] items) {
+
         return Arrays.stream(items)
-            .flatMap(item -> Arrays.stream(new String[] {item,item}))
+            .flatMap(item -> Arrays.stream(new String[] {item, item}))
             .toArray(String[]::new);
     }
-    public static String[][] enlargeArrayImage(String[][] image){
+
+    public static String[][] enlargeArrayImage(String[][] image) {
+
         String[][] horizontalyStretched = Arrays.stream(image)
-            .map(App::DuplicateValue)
+            .map(App::duplicateValues)
             .toArray(String[][]::new);
-            
-            return Arrays.stream(horizontalyStretched)
-                .flarmap(item -> Arrays.stream(new String[][] {item, item}))
-                .toArray(String[][]::new);
+
+        return Arrays.stream(horizontalyStretched)
+            .flatMap(item -> Arrays.stream(new String[][] {item, item}))
+            .toArray(String[][]::new);
     }
 }
-// END
--- java-collections-ru/lambdas/src/main/java/exercise/Sorter.java
+++ java-collections-ru/lambdas/src/main/java/exercise/Sorter.java
@@ -7,17 +7,16 @@ import java.util.List;
 import java.time.LocalDate;
 import java.util.stream.Collectors;
 
-public class Sorter {
+class Sorter {
     public static List<String> takeOldestMans2(List<Map<String, String>> users) {
         return users.stream()
-            .filter(user -> "male".equals(user.get("gender")))
+            .filter(user -> user.get("gender") == "male")
             .sorted((user1, user2) -> {
-                LocalDate date1 = LocalDate.parse(user1.get("Birthday"));
-                LocalDate date2 = LocalDate.parse(user2.get("Birthday"));
+                LocalDate date1 = LocalDate.parse(user1.get("birthday"));
+                LocalDate date2 = LocalDate.parse(user2.get("birthday"));
                 return date1.compareTo(date2);
             })
             .map(user -> user.get("name"))
             .collect(Collectors.toList());
     }
 }
-

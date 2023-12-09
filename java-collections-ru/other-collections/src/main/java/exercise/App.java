7856c6881fe810984619cae1754dfe338aa902c0
@hexlet/cli <support@hexlet.io>
2023-12-09 17:18:48 +0300

    submit java-collections-ru/other-collections

--- java-collections-ru/other-collections/src/main/java/exercise/App.java
+++ java-collections-ru/other-collections/src/main/java/exercise/App.java
@@ -6,25 +6,28 @@ import java.util.Set;
 import java.util.TreeSet;
 
 // BEGIN
-class App {
-    public static Map<String, String> genDiff(Map<String, Object> data1,Map<String, Object> data2) {
-        Set<String> allKeys= new TreeSet<>(data1.keySet());
+public class App {
+    public static Map<String, String> genDiff(Map<String, Object> data1, Map<String, Object> data2) {
+        Set<String> allKeys = new TreeSet<>(data1.keySet());
         allKeys.addAll(data2.keySet());
+
         Map<String, String> result = new LinkedHashMap<>();
-        for (String key : allKeys){
-            if(data1.containsKey(key) && data2.containsKey(key)){
-                result.put(key,"unchanged");
-            }
-            else {
-                result.put(key, "changed");
-            }
-            else if (data1.containsKey(key)) {
+
+        for (String key : allKeys) {
+            if (data1.containsKey(key) && data2.containsKey(key)) {
+                if (Objects.equals(data1.get(key), data2.get(key))) {
+                    result.put(key, "unchanged");
+                } else {
+                    result.put(key, "changed");
+                }
+            } else if (data1.containsKey(key)) {
                 result.put(key, "deleted");
             } else if (data2.containsKey(key)) {
                 result.put(key, "added");
             }
         }
-        return result
+
+        return result;
     }
 }
 //END

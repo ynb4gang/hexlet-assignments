a0dcb03bcf0c5bd482baa6fb17a44beb0d13d5ca
@hexlet/cli <support@hexlet.io>
2024-01-24 19:35:09 +0300

    submit java-oop-ru/interfaces

--- java-oop-ru/interfaces/src/main/java/exercise/App.java
+++ java-oop-ru/interfaces/src/main/java/exercise/App.java
@@ -1,20 +1,9 @@
 class App {
-
-    // new
     public static List<String> buildApartmentsList(List<Home> apartments, int count) {
         return apartments.stream()
             .sorted(Home::compareTo)
             .limit(count)
             .map(Home::toString)
             .toList();
-    }
-    // old
-    public static List<String> buildApartmentsList2(List<Home> apartments, int count) {
-        int normalizedCount = Math.min(count, apartments.size());
-        apartments.sort(Home::compareTo);
-        List<Home> sublist = apartments.subList(0, normalizedCount);
-        return sublist.stream()
-            .map(appartment -> appartment.toString())
-            .collect(Collectors.toList());
     }
 }

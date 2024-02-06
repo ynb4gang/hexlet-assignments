68ea04d2a4ba4192eddfc8c5892a33b814b48ce1
@hexlet/cli <support@hexlet.io>
2024-02-06 14:17:34 +0300

    submit java-oop-ru/reflections

---
+++ java-oop-ru/reflections/src/main/java/exercise/Validator.java
@@ -0,0 +1,69 @@
+package exercise;
+
+import java.lang.reflect.Field;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.stream.Collectors;
+
+class Validator {
+    public static List<String> validate(Object instance) {
+        List<Field> fields = List.of(instance.getClass().getDeclaredFields());
+        return fields.stream()
+            .filter(field -> field.isAnnotationPresent(NotNull.class))
+            .filter(field -> {
+                Object value;
+                try {
+                    field.setAccessible(true);
+                    value = field.get(instance);
+                    field.setAccessible(false);
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
+                return value == null;
+            })
+            .map(Field::getName)
+            .collect(Collectors.toList());
+    }
+
+    public static Map<String, List<String>> advancedValidate(Object instance) {
+        List<Field> fields = List.of(instance.getClass().getDeclaredFields());
+        Map<String, List<String>> validationErrors = new HashMap<>();
+        fields.stream()
+            .filter(field -> field.isAnnotationPresent(NotNull.class) || field.isAnnotationPresent(MinLength.class))
+            .forEach(field -> {
+                String fieldName = field.getName();
+                List<String> errors = getErrorsForField(field, instance);
+                if (!errors.isEmpty()) {
+                    validationErrors.put(fieldName, errors);
+                }
+            });
+        return validationErrors;
+    }
+
+    public static List<String> getErrorsForField(Field field, Object instance) {
+        List<String> errors = new ArrayList<>();
+        String value;
+
+        try {
+            field.setAccessible(true);
+            value = (String) field.get(instance);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        if (field.isAnnotationPresent(NotNull.class) && value == null) {
+            errors.add("can not be null");
+        }
+
+        if (field.isAnnotationPresent(MinLength.class)) {
+            int minLength = field.getAnnotation(MinLength.class).minLength();
+            if (value == null || value.length() < minLength) {
+                errors.add("length less than " + minLength);
+            }
+        }
+        return errors;
+    }
+}
+// END
---
+++ java-oop-ru/reflections/src/test/java/exercise/ValidationTest.java
@@ -0,0 +1,47 @@
+package exercise;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+import java.util.List;
+import java.util.Map;
+
+
+
+class ValidationTest {
+
+    @Test
+    void testValidate() {
+        Address address1 = new Address("Russia", "Ufa", "Lenina", "54", null);
+        List<String> result1 = Validator.validate(address1);
+        List<String> expected1 = List.of();
+        assertThat(result1).isEqualTo(expected1);
+
+        Address address2 = new Address(null, "London", "1-st street", "5", "1");
+        List<String> result2 = Validator.validate(address2);
+        List<String> expected2 = List.of("country");
+        assertThat(result2).isEqualTo(expected2);
+
+        Address address3 = new Address("USA", null, null, null, "1");
+        List<String> result3 = Validator.validate(address3);
+        List<String> expected3 = List.of("city", "street", "houseNumber");
+        assertThat(result3).isEqualTo(expected3);
+    }
+
+    // BEGIN
+    @Test
+    void testAdvancedValidate() {
+        Address address1 = new Address("Russia", "Ufa", "Lenina", "54", null);
+        Map<String, List<String>> result1 = Validator.advancedValidate(address1);
+        Map<String, List<String>> expected1 = Map.of();
+        assertThat(result1).isEqualTo(expected1);
+
+        Address address2 = new Address("Ru", "Moscow", null, "54", "10");
+        Map<String, List<String>> result2 = Validator.advancedValidate(address2);
+        Map<String, List<String>> expected2 = Map.of(
+            "country", List.of("length less than 3"),
+            "street", List.of("can not be null")
+        );
+        assertThat(result2).isEqualTo(expected2);
+    }
+    // END
+}

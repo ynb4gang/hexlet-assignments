90a683be30d7b9311fe0cf271fe6d3679d7097b2
@hexlet/cli <support@hexlet.io>
2024-05-19 14:51:34 +0300

    submit java-spring-ru/http-crud

--- java-spring-ru/http-crud/src/main/java/exercise/Application.java
+++ java-spring-ru/http-crud/src/main/java/exercise/Application.java
@@ -31,15 +31,8 @@ public class Application {
     public List<Post> index(
         @RequestParam(defaultValue = "1") Integer page,
         @RequestParam(defaultValue = "10") Integer limit) {
-        return posts.stream().skip((page - 1) * limit).limit(limit).toList();
-    }
 
-    @GetMapping("/posts/{id}")
-    public Optional<Post> show(@PathVariable String id) {
-        var post = posts.stream()
-                        .filter(p -> p.getId().equals(id))
-                        .findFirst();
-        return post;
+        return posts.stream().skip((page - 1) * limit).limit(limit).toList();
     }
 
     @PostMapping("/posts")
@@ -48,23 +41,31 @@ public class Application {
         return post;
     }
 
-    @PutMapping("/posts/{id}") 
-    static Post update(@RequestParam String id, @RequestParam Post data){
+    @GetMapping("/posts/{id}")
+    public Optional<Post> show(@PathVariable String id) {
         var post = posts.stream()
-                        .filter(p -> p.getId().equals(id))
-                        .findFirst();
-        if (post.isPresent()) {
-            var post1 = post.get();
-            post1.setId(data.getId());
-            post1.setTitle(data.getTitle());
-            post1.setBody(data.getbody());
+            .filter(p -> p.getId().equals(id))
+            .findFirst();
+        return post;
+    }
+
+    @PutMapping("/posts/{id}")
+    public Post update(@PathVariable String id, @RequestBody Post data) {
+        var maybePost = posts.stream()
+            .filter(p -> p.getId().equals(id))
+            .findFirst();
+        if (maybePost.isPresent()) {
+            var post = maybePost.get();
+            post.setId(data.getId());
+            post.setTitle(data.getTitle());
+            post.setBody(data.getBody());
         }
         return data;
     }
 
-    @DeleteMapping("/pages/{id}")
+    @DeleteMapping("/posts/{id}")
     public void destroy(@PathVariable String id) {
-        posts.removeIf(p ->p.getId().equals(id));
+        posts.removeIf(p -> p.getId().equals(id));
     }
     // END
 }

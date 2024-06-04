8623664539e8e963a69393bfd29ae45a9fa1dc95
@hexlet/cli <support@hexlet.io>
2024-06-04 16:44:59 +0300

    submit java-spring-ru/jpa-data

--- java-spring-ru/jpa-data/src/main/java/exercise/controller/PeopleController.java
+++ java-spring-ru/jpa-data/src/main/java/exercise/controller/PeopleController.java
@@ -38,7 +38,7 @@ public class PeopleController {
 
     @PostMapping("")
     @ResponseStatus(HttpStatus.CREATED)
-    public Person create(@Valid @RequestBody Person person) {
+    public Person create(@RequestBody Person person) {
         return personRepository.save(person);
     }
 
